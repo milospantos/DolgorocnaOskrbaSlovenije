4ee56a96af24bc2541e03247c9de74d0755999ba
milos.pantos <milos.pantos@infinitas.si>
2025-05-22 09:26:03 +0200

    Dodana main.py datoteka

---
+++ src/o32_dolgorocnaoskrbaslovenije_maj25/main.py
@@ -0,0 +1,20 @@
+# src/o31_nepremicnine_maj25/main.py
+import pandas as pd
+
+def pozdrav():
+    print("Lep pozdrav!")
+    print("Lep pozdrav še enkrat!")
+
+if __name__ == "__main__":
+    pozdrav()
+    x=4
+    y=5
+    z=x+y
+    Z=pd.DataFrame([z])
+    print('Dober dan!')
+    print(z)
+    print('Dober večer')
+
+
+rp=1
+rp2=1
